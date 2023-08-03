02c3d1279022bd6b323ca861531a673a5a68f355
Remzi Kurshumliu <admin@remzik.altostrat.com>
2023-08-03 11:00:02 +0000

    dd

---
+++ includes/mapping.js
@@ -0,0 +1,7 @@
+function professionGroup(Profession) {
+  return `CASE
+          WHEN ${Profession} IN ("CE", "PA") THEN "Customer Engineering"
+          ELSE "Other"
+          END`;
+}
+module.exports = { professionGroup };
